ea72f229563ab882371506d098dd6ec4e0581172
robincamarasaphd <r.camarasa@erasmusmc.nl>
2020-03-02 16:58:48 +0100

    Numpy : Created numpy solutions

--- src/exercices.py
+++ src/exercices.py
@@ -5,7 +5,7 @@ def ex_1() -> np.ndarray:
     """
     En une ligne, creer un tableau de dimension (3, 4, 5).
     """
-    pass
+    return np.arange(3 * 4 * 5).reshape(3, 4, 5)
 
 
 def ex_2(arr: np.ndarray) -> np.ndarray:
@@ -14,7 +14,7 @@ def ex_2(arr: np.ndarray) -> np.ndarray:
 
     :param arr: Numpy array
     """
-    pass
+    return np.where(arr%2 == 0)
 
 
 def ex_3(n: int) -> np.ndarray:
@@ -23,22 +23,28 @@ def ex_3(n: int) -> np.ndarray:
 
     :param n: Nombre
     """
-    pass
+    result = np.eye(n+1)
+    for i in range(1, n+1):
+        result[i, :] = result[i-1, :] + np.concatenate((np.array([0]), result[i-1, :-1]))
+    return result
 
 
-def ex_4(A, B):
+def ex_4(A: np.ndarray, B: np.ndarray):
     """
     Ecrire une fonction de résolution de systèmes linéaires de la forme AX = B.
 
     :param A: Matrice carrée contenant les paramètres du système
     :param B: Vector colonne contenant les paramètres du système
     """
-    pass
+    return np.dot(np.linalg.inv(A), B)
 
 
 def ex_5():
     """
     Chargez le fichier data/data.npy mettez l’ensemble de ces valeursau carr ́e sauvegardez le tableau dans le fichier data/datasquared.npy
     """
-    pass
+    arr = np.load('../data/data.npy')
+    arr = arr ** 2
+    np.save('../data/data_squared.npy', arr)
+
 
