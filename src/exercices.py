ddc343a6e10b7af915f73fd3eaf46d9ca598fe59
robincamarasaphd <r.camarasa@erasmusmc.nl>
2020-03-02 17:26:25 +0100

    Condition and loops : Add solutions

--- src/exercices.py
+++ src/exercices.py
@@ -7,7 +7,8 @@ def ex_1(n: int) -> np.ndarray:
 
     :param n: Un nombre
     """
-    pass
+    for i in range(1, n+1):
+        print(i)
 
 
 def ex_2(n: int) -> np.ndarray:
@@ -16,7 +17,8 @@ def ex_2(n: int) -> np.ndarray:
 
     :param n: Un nombre
     """
-    pass
+    for i in range(n, 0, -1):
+        print(i)
 
 
 def ex_3(n: int) -> np.ndarray:
@@ -25,7 +27,10 @@ def ex_3(n: int) -> np.ndarray:
 
     :param n: Un nombre
     """
-    pass
+    acc = 1
+    for i in range(1, n+1):
+        acc *= i
+    return acc
 
 
 def ex_4(age):
@@ -34,5 +39,15 @@ def ex_4(age):
 
     :param age: Age de la personne consideree
     """
-    pass
+    if age in range(100):
+        if age < 18:
+            print('Mineur')
+        elif age < 23:
+            print('Etudiant')
+        elif age < 62:
+            print('Travailleur')
+        else:
+            print('Retraite')
+    else:
+        print('Age invalide')
 
