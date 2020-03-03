93212f7b61f6f949fc21ae71a4ee87ea4ae752a5
robincamarasaphd <r.camarasa@erasmusmc.nl>
2020-03-03 22:03:42 +0100

    Lists and strings : Add solutions

--- src/exercices.py
+++ src/exercices.py
@@ -5,7 +5,8 @@ def ex_1(c: str) -> int:
     """
     Créer une fonction qui renvoie la position dans l’alphabet d’un charactère.
     """
-    pass
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return alphabet.index(c) + 1
 
 
 def ex_2(s: str) -> list:
@@ -13,7 +14,10 @@ def ex_2(s: str) -> list:
     En s’aidant de l’exercice 1, créer une fonction qui renvoie la liste des positions dans
     l’alphabet des charactères d’une chaîne de charactère.
     """
-    pass
+    result = []
+    for c in s:
+        result.append(ex_1(c))
+    return result
 
 
 def ex_3(n: int) -> list:
@@ -23,5 +27,16 @@ def ex_3(n: int) -> list:
 
     :param n: Nombre
     """
-    pass
+    prime = [True] * (n+1)
+    prime[0] = False
+    prime[1] = False
+    result = []
+    for i in range(2, n+1):
+        if prime[i]:
+            for j in range(2*i, n+1, i):
+                prime[j] = False
+    for i, item in enumerate(prime):
+        if item:
+            result.append(i)
+    return result
 
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     ]
     arguments = [
         'n',
-        ('Hello',),
+        ('hello',),
         (100,)
     ]
 
