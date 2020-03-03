de01e4b1f1e1e57c59b089ba1c5b3f340bf500d0
robincamarasaphd <r.camarasa@erasmusmc.nl>
2020-03-03 22:22:00 +0100

    Pandas : Add solutions

--- src/exercices.py
+++ src/exercices.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -5,7 +6,9 @@ def ex_1() -> pd.DataFrame:
     """
     Mettre les donnees contenues dans data/id.npy et data/height.npy sous forme de dataframe.
     """
-    pass
+    id = np.load('../data/id.npy')
+    heights = np.load('../data/height.npy')
+    return pd.DataFrame(data= {'id': id, 'heights': heights})
 
 
 def ex_2() -> None:
@@ -14,5 +17,8 @@ def ex_2() -> None:
     en supprimant les lignes ayant des valeurs nulles. Exportez ensuite le dataframe
     obtenu dans un fichier output/data.csv.
     """
-    pass
+    df = pd.read_csv('../data/nom.csv')
+    df2 = df.join(ex_1().set_index('id'), on='id')
+    df2.dropna(inplace=True)
+    df2.to_csv('../output/data.csv')
 
